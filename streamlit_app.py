0407a49e76626855a89649c251e6190ada342058
Rakshanda-Sawaji <rakshanda.sawaji@eversana.com>
2025-05-09 01:13:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -14,14 +13,14 @@ name_on_order =st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+cnn = st.connection("snowflake")
+session = cnn.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 
 
 ingredients_list = st.multiselect(
-    'Choose uo to 5 ingredients:'
+    'Choose up to 5 ingredients:'
     , my_dataframe
     ,max_selections=5
 )
@@ -44,6 +43,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
     
-
-st.secrets
 
