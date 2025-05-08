cce03dc0cefe4ddf974ef64ee635b1f34ad3406f
Rakshanda-Sawaji <rakshanda.sawaji@eversana.com>
2025-05-09 02:30:41 +0530

    Update streamlit_app.py
    Added pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
-import pandas
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -20,6 +20,11 @@ session = cnn.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+
+#Convert the snowpark dataframe to a panda dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 
