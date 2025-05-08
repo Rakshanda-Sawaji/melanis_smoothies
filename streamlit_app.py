595ef8fde7574bc1859bd8f9aa9552b450d17327
Rakshanda-Sawaji <rakshanda.sawaji@eversana.com>
2025-05-09 00:09:20 +0530

    Update streamlit_app.py
    Removed unnecessary code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,7 +9,7 @@ streamlit.write(
   """
 )
 
-#name_on_order =st.text_input('Name on Smoothie:')
+
 streamlit.write('Omega 3 & Blueberry Oatmeal')
 streamlit.write('Kale, Spinach & Rocket Smoothie')
 streamlit.write('Hard-Boiled Free-Range Egg')
@@ -18,17 +17,12 @@ streamlit.write('Hard-Boiled Free-Range Egg')
 cnx = st.connection("Snowflake")
 session = cnx.session()
 
-#session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 
 
 
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
-    time_to_insert = st.button('Submit Order')
 
-#st.write(my_insert_stmt)
+
     
    
     
